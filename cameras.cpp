b5f81674d68dae503be8d8a1aed961ac9cb13702
lucas8 <luc.linux@mailoo.org>
2014-04-06 11:50:36 +0200

    Updated Cameras class : only one camera input is now needed.

--- cameras.cpp
+++ cameras.cpp
@@ -3,35 +3,15 @@
 
 namespace libcv
 {
-    CamException::CamException(const char* msg) noexcept
-        : m_msg(msg)
-    {}
-
-    const char* CamException::what() const noexcept
+    Cameras::Cameras(int l, bool delay)
+        : m_cpt(NULL), m_img(NULL), m_loops(0)
     {
-        return m_msg;
-    }
+        m_cpt = cvCaptureFromCAM(l);
+        if(!m_cpt)
+            return;
 
-    Cameras::Cameras(int l, int r, bool delay)
-        : m_left(NULL), m_right(NULL), m_gleft(NULL), m_gright(NULL), m_loops(0)
-    {
-        m_left  = cvCaptureFromCAM(l);
-        m_right = cvCaptureFromCAM(r);
-        if(!m_left)
-            throw CamException("Couldn't open left camera.");;
-        if(!m_right)
-            throw CamException("Couldn't open right camera.");
-
-        m_size.width  = cvGetCaptureProperty(m_left, CV_CAP_PROP_FRAME_WIDTH);
-        m_size.height = cvGetCaptureProperty(m_left, CV_CAP_PROP_FRAME_HEIGHT);
-        if(m_size.width != cvGetCaptureProperty(m_right, CV_CAP_PROP_FRAME_WIDTH)
-                    || m_size.height != cvGetCaptureProperty(m_right, CV_CAP_PROP_FRAME_HEIGHT))
-            throw CamException("The left and right cameras doesn't have the same size.");
-
-        m_gleft  = cvCreateImage(m_size, IPL_DEPTH_8U, 1);
-        m_gright = cvCreateImage(m_size, IPL_DEPTH_8U, 1);
-        if(!m_gleft || !m_gright)
-            throw CamException("Couldn't load internal image buffers in Cameras.");
+        m_size.width  = cvGetCaptureProperty(m_cpt, CV_CAP_PROP_FRAME_WIDTH);
+        m_size.height = cvGetCaptureProperty(m_cpt, CV_CAP_PROP_FRAME_HEIGHT);
 
         if(delay)
             m_loops = 10;
@@ -41,41 +21,30 @@ namespace libcv
 
     Cameras::~Cameras()
     {
-        if(m_left)
-            cvReleaseCapture(&m_left);
-        if(m_right)
-            cvReleaseCapture(&m_right);
-        if(m_gleft)
-            cvReleaseImage(&m_gleft);
-        if(m_gright)
-            cvReleaseImage(&m_gright);
+        if(m_cpt)
+            cvReleaseCapture(&m_cpt);
+    }
+            
+    bool Cameras::loaded() const
+    {
+        return m_cpt != NULL;
     }
 
     bool Cameras::queryFrames()
     {
         /* Clear the buffered images and store the last one */
         for(int i = 0; i < m_loops; ++i) {
-            if(!cvGrabFrame(m_left))
-                return false;
-            if(!cvGrabFrame(m_right))
+            if(!cvGrabFrame(m_cpt))
                 return false;
         }
 
-        IplImage* tmp = cvRetrieveFrame(m_left);
-        cvCvtColor(tmp, m_gleft, CV_BGR2GRAY);
-        tmp = cvRetrieveFrame(m_right);
-        cvCvtColor(tmp, m_gright, CV_BGR2GRAY);
+        m_img = cvRetrieveFrame(m_cpt);
         return true;
     }
 
-    IplImage* Cameras::left() const
+    IplImage* Cameras::get() const
     {
-        return m_gleft;
-    }
-
-    IplImage* Cameras::right() const
-    {
-        return m_gright;
+        return m_img;
     }
 
     CvSize Cameras::size() const
--- cameras.hpp
+++ cameras.hpp
@@ -9,37 +9,24 @@
 
 namespace libcv
 {
-    class CamException : public std::exception
-    {
-        public:
-            CamException() = delete;
-            explicit CamException(const char* msg) noexcept;
-            virtual const char* what() const noexcept;
-
-        private:
-            const char* m_msg;
-    };
-
     class Cameras
     {
         public:
-            Cameras(int l, int r, bool delay = true);
+            Cameras(int l, bool delay = true);
             Cameras() = delete;
             Cameras(const Cameras&) = delete;
             ~Cameras();
+            bool loaded() const;
 
             /* To call once in each iteration of main loop */
             bool queryFrames();
             /* Get the pictures (return mustn't be free'd */
-            IplImage* left() const;
-            IplImage* right() const;
+            IplImage* get() const;
             CvSize size() const;
 
         private:
-            CvCapture* m_left;
-            CvCapture* m_right;
-            IplImage* m_gleft;
-            IplImage* m_gright;
+            CvCapture* m_cpt;
+            IplImage* m_img;
             CvSize m_size;
             int m_loops;
     };
